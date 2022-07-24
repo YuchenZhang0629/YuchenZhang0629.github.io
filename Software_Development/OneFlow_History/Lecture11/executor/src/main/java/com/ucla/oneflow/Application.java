00c977b4706e31f2ba861e5b237641bc0b5ada58
YuchenZhang0629 <yuchenz0629@g.ucla.edu>
2022-07-24 00:39:19 -0700

    New Backup

---
+++ Software_Development/OneFlow_History/Lecture11/executor/src/main/java/com/ucla/oneflow/Application.java
@@ -0,0 +1,73 @@
+package com.ucla.oneflow;
+
+import com.ucla.oneflow.common.utils.ConfigUtil;
+import com.ucla.oneflow.executor.executorjob.ExecutorJob;
+import com.ucla.oneflow.executor.job.HiveJob;
+import com.ucla.oneflow.executor.listener.OrderListener;
+import com.ucla.oneflow.executor.service.HiveService;
+import com.ucla.oneflow.model.JobDescriptor;
+import com.ucla.oneflow.model.VO.ConfigVO;
+import com.ucla.oneflow.model.VO.StepVO;
+import org.quartz.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+@SpringBootApplication
+public class Application {
+
+
+    @Autowired
+    private ConfigUtil configUtil;
+    @Autowired
+    private Scheduler scheduler;
+
+    private static final Logger appLogger = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @PostConstruct
+    public void run() throws FileNotFoundException {
+        // Adding a logger to track the pipeline's operation
+        // We also have debug, warning, and error
+        appLogger.info("OneFlow starting to operate");
+        ConfigVO configVO = configUtil.getConfigVO();
+        appLogger.info("Get ConfigVO");
+        List<ConfigVO.DataSource> dataSources = configVO.getDatasources();
+        appLogger.info("Get Data Source Config");
+        configVO.getTasks().forEach(taskVO -> {
+            String taskId = taskVO.getTaskId();
+            String taskName = taskVO.getTaskName();
+            String cron = taskVO.getCron();
+            String group = taskVO.getGroup();
+            JobDescriptor jobDescriptor = new JobDescriptor();
+            jobDescriptor.setGroup(group);
+            jobDescriptor.setName(taskName);
+            jobDescriptor.setJobClazz(ExecutorJob.class);
+            Map<String,Object> paramMap = new HashMap<>();
+            paramMap.put("taskName",taskName);
+            paramMap.put("taskId",taskId);
+            paramMap.put("cron ",cron);
+            paramMap.put("taskVO ",taskVO);
+            paramMap.put("group",group);
+            jobDescriptor.setDataMap(paramMap);
+            JobDetail executorJobDetail = jobDescriptor.buildJobDetail();
+
+            Trigger jobTrigger = TriggerBuilder.newTrigger().withIdentity(taskName,group).
+                    withSchedule(CronScheduleBuilder.cronSchedule(cron)).build();
+
+            try {
+                scheduler.scheduleJob(executorJobDetail,jobTrigger);
+            } catch (SchedulerException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
---
+++ Software_Development/OneFlow_History/Lecture11/executor/src/main/java/com/ucla/oneflow/executor/executorjob/ExecutorJob.java
@@ -0,0 +1,99 @@
+package com.ucla.oneflow.executor.executorjob;
+
+import com.ucla.oneflow.Application;
+import com.ucla.oneflow.executor.job.HiveJob;
+import com.ucla.oneflow.executor.listener.OrderListener;
+import com.ucla.oneflow.model.JobDescriptor;
+import com.ucla.oneflow.model.VO.StepVO;
+import com.ucla.oneflow.model.VO.TaskVO;
+import org.quartz.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+
+public class ExecutorJob implements Job {
+    private static final Logger appLogger = LoggerFactory.getLogger(ExecutorJob.class);
+    @Autowired
+    private Scheduler scheduler;
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        JobDataMap paramMap = jobExecutionContext.getMergedJobDataMap();
+        String taskName = paramMap.getString("taskName");
+        String taskId = paramMap.getString("taskId");
+        TaskVO taskVO = (TaskVO) paramMap.get("taskVO");
+        String group = paramMap.getString("group");
+        OrderListener orderListener = new OrderListener(taskName + " OrderListener");
+
+
+        appLogger.info("Get task" + taskId + " 's Config");
+        taskVO.getSteps().sort(new Comparator<StepVO>() {
+            @Override
+            public int compare(StepVO o1, StepVO o2) {
+                return Integer.parseInt(o1.getOrder())-Integer.parseInt(o2.getOrder());
+            }
+        });
+        Queue<JobDetail> jobDetailQueue = new LinkedList<>();
+        // Use a hashmap to check whether we have finished step1
+        // If step1 and step2 have the same order hierarchy
+        Map<String,String> statusMap = new HashMap<>();
+        taskVO.getSteps().forEach(stepVO -> {
+            appLogger.info("Get step" + stepVO.getOrder() + " 's Config");
+            statusMap.put(stepVO.getStepName(),"new");
+            String type = stepVO.getType();
+            JobDescriptor jobDescriptor = new JobDescriptor();
+            jobDescriptor.setGroup(group);
+            jobDescriptor.setName(taskName);
+            jobDescriptor.setName(stepVO.getStepName());
+            paramMap.put("order",stepVO.getOrder());
+            paramMap.put("stepName",stepVO.getStepName());
+            if (type.equals("hive")) {
+                jobDescriptor.setJobClazz(HiveJob.class);
+                paramMap.put("path",stepVO.getPath());
+                paramMap.put("hiveParam", stepVO.getHiveParam());
+            } else if (type.equals("spark")) {
+                // jobDescriptor.setJobClazz(ScriptJob.class);
+                paramMap.put("path",stepVO.getPath());
+                paramMap.put("hiveParam", stepVO.getParam());
+                paramMap.put("mode", stepVO.getMode());
+            } else if (type.equals("script")) {
+                jobDescriptor.setJobClazz(SparkJob.class);
+                paramMap.put("path",stepVO.getPath());
+                paramMap.put("master", stepVO.getMaster());
+                paramMap.put("deployMode", stepVO.getDeployMode());
+                paramMap.put("className", stepVO.getClassName());
+                paramMap.put("sparkLogPath",stepVO.getSparkLogPath());
+            }
+            jobDescriptor.setDataMap(paramMap);
+            JobDetail jobDetail =jobDescriptor.buildJobDetail();
+            jobDetailQueue.add(jobDetail);
+        });
+        // Add another queue to check whether there is another step
+        // with the same order hierarchy
+        List<String> runningJobList = new ArrayList<>();
+        orderListener.setJobDetailQueue(jobDetailQueue);
+        orderListener.setStatusMap(statusMap);
+        orderListener.setRunningJobList(runningJobList);
+        try {
+            scheduler.getListenerManager().addJobListener(orderListener);
+            if (jobDetailQueue.isEmpty()) {
+                appLogger.warn(taskName + "'s Step List is empty");
+                return;
+            }
+            while (!jobDetailQueue.isEmpty() && jobDetailQueue.peek().getJobDataMap().getString("order").equals("1")){
+                // When the task start working, update the status to "processing"
+                JobDetail initJobDetail = jobDetailQueue.poll();
+                String stepName = initJobDetail.getJobDataMap().getString("stepName");
+                runningJobList.add(stepName);
+                statusMap.put(stepName,"processing");
+                appLogger.info(stepName + " is running");
+                scheduler.addJob(initJobDetail,true);
+                scheduler.triggerJob(initJobDetail.getKey());
+            }
+        } catch (Exception e) {
+            appLogger.error("Error",e);
+        }
+    }
+}
